00ba3da7248c2326ef05ec8b7ffc0bbd40b2e41c
Toxe <toxe@toxe.net>
2019-08-09 12:54:39 +0200

    Remove unnecessary Maze:: prefix

--- maze_generator.cpp
+++ maze_generator.cpp
@@ -28,26 +28,26 @@ public:
     Maze(const int width, const int height)
         : width_{width},
           height_{height},
-          nodes_(width * height, static_cast<Node>(Maze::WallFlags::North) | static_cast<Node>(Maze::WallFlags::East) | static_cast<Node>(Maze::WallFlags::South) | static_cast<Node>(Maze::WallFlags::West)) {}
+          nodes_(width * height, static_cast<Node>(WallFlags::North) | static_cast<Node>(WallFlags::East) | static_cast<Node>(WallFlags::South) | static_cast<Node>(WallFlags::West)) {}
 
     int width() const { return width_; }
     int height() const { return height_; }
 
-    bool valid_coords(const Maze::Coordinates coords) const { return coords.x >= 0 && coords.y >= 0 && coords.x < width_ && coords.y < height_; }
+    bool valid_coords(const Coordinates coords) const { return coords.x >= 0 && coords.y >= 0 && coords.x < width_ && coords.y < height_; }
 
-    Maze::Coordinates coords_in_direction(const Maze::Coordinates coords, const Maze::Directions dir) {
-        const Maze::Coordinates offset{direction_coords_offset_[static_cast<int>(dir)]};
-        return Maze::Coordinates{coords.x + offset.x, coords.y + offset.y};
+    Coordinates coords_in_direction(const Coordinates coords, const Directions dir) {
+        const Coordinates offset{direction_coords_offset_[static_cast<int>(dir)]};
+        return Coordinates{coords.x + offset.x, coords.y + offset.y};
     }
 
-    bool node_visited(const Maze::Coordinates coords) const { return nodes_[coords.y * width_ + coords.x] & 0b10000; }
-    void set_node_visited(const Maze::Coordinates coords) { nodes_[coords.y * width_ + coords.x] |= 0b10000; }
+    bool node_visited(const Coordinates coords) const { return nodes_[coords.y * width_ + coords.x] & 0b10000; }
+    void set_node_visited(const Coordinates coords) { nodes_[coords.y * width_ + coords.x] |= 0b10000; }
 
-    bool has_wall(const Maze::Coordinates coords, Maze::WallFlags wall) const { return nodes_[coords.y * width_ + coords.x] & static_cast<Node>(wall); }
+    bool has_wall(const Coordinates coords, WallFlags wall) const { return nodes_[coords.y * width_ + coords.x] & static_cast<Node>(wall); }
 
-    void clear_walls(const Maze::Coordinates orig, const Maze::Coordinates dest, Maze::Directions dir) {
-        const Maze::WallFlags orig_wall = wall_in_direction_[static_cast<int>(dir)];
-        const Maze::WallFlags dest_wall = wall_in_direction_[static_cast<int>(opposite_direction_[static_cast<int>(dir)])];
+    void clear_walls(const Coordinates orig, const Coordinates dest, Directions dir) {
+        const WallFlags orig_wall = wall_in_direction_[static_cast<int>(dir)];
+        const WallFlags dest_wall = wall_in_direction_[static_cast<int>(opposite_direction_[static_cast<int>(dir)])];
         nodes_[orig.y * width_ + orig.x] &= ~(static_cast<Node>(orig_wall));
         nodes_[dest.y * width_ + dest.x] &= ~(static_cast<Node>(dest_wall));
     }
@@ -57,9 +57,9 @@ private:
     const int height_;
     std::vector<Node> nodes_;
 
-    const Maze::WallFlags wall_in_direction_[4] = { Maze::WallFlags::North, Maze::WallFlags::East, Maze::WallFlags::South, Maze::WallFlags::West };
-    const Maze::Directions opposite_direction_[4] = { Maze::Directions::South, Maze::Directions::West, Maze::Directions::North, Maze::Directions::East };
-    const Maze::Coordinates direction_coords_offset_[4] = { Maze::Coordinates{0, -1}, Maze::Coordinates{1, 0}, Maze::Coordinates{0, 1}, Maze::Coordinates{-1, 0} };
+    const WallFlags wall_in_direction_[4] = { WallFlags::North, WallFlags::East, WallFlags::South, WallFlags::West };
+    const Directions opposite_direction_[4] = { Directions::South, Directions::West, Directions::North, Directions::East };
+    const Coordinates direction_coords_offset_[4] = { Coordinates{0, -1}, Coordinates{1, 0}, Coordinates{0, 1}, Coordinates{-1, 0} };
 };
 
 void print(Maze& maze)
