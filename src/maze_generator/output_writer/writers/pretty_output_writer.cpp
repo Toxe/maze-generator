28920d08815d703c67ebe6523747bd5cf56bc753
Toxe <toxe@toxe.net>
2022-10-01 10:00:49 +0200

    Simplify code

--- src/maze_generator/output_writer/writers/pretty_output_writer.cpp
+++ src/maze_generator/output_writer/writers/pretty_output_writer.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 #include "pretty_output_writer.hpp"
 
 namespace maze_generator::output_writer {
@@ -7,14 +9,14 @@ void PrettyOutputWriter::output_maze(maze::Maze& maze, int)
     const auto size = maze.size();
 
     // create grid with wall data
-    const int grid_width = 2 * size.width + 1;
-    const int grid_height = 2 * size.height + 1;
-    std::vector<std::vector<maze::Node>> grid(static_cast<std::size_t>(grid_height));
+    const std::size_t grid_width = 2 * static_cast<std::size_t>(size.width) + 1;
+    const std::size_t grid_height = 2 * static_cast<std::size_t>(size.height) + 1;
+    std::vector<std::vector<maze::Node>> grid(grid_height);
 
-    for (std::size_t row = 0; row < static_cast<std::size_t>(grid_height); ++row) {
-        grid[row].reserve(static_cast<std::size_t>(grid_width));
+    for (std::size_t row = 0; row < grid_height; ++row) {
+        grid[row].reserve(grid_width);
 
-        for (std::size_t col = 0; col < static_cast<std::size_t>(grid_width); ++col)
+        for (std::size_t col = 0; col < grid_width; ++col)
             grid[row].push_back(maze::Node{});
     }
 
@@ -48,27 +50,30 @@ void PrettyOutputWriter::output_maze(maze::Maze& maze, int)
         }
     }
 
-    for (int row = 0; row < grid_height; ++row) {
-        for (int col = 0; col < grid_width; ++col) {
-            switch (grid[static_cast<std::size_t>(row)][static_cast<std::size_t>(col)].value()) {
-                case 0b0000: write(reinterpret_cast<const char*>(u8"  ")); break;  // |  |
-                case 0b0001: write(reinterpret_cast<const char*>(u8"\u2500 ")); break;  // |─ |
-                case 0b0010: write(reinterpret_cast<const char*>(u8"\u2577 ")); break;  // |╷ |
-                case 0b0011: write(reinterpret_cast<const char*>(u8"\u2510 ")); break;  // |┐ |
-                case 0b0100: write(reinterpret_cast<const char*>(u8" \u2500")); break;  // | ─|
-                case 0b0101: write(reinterpret_cast<const char*>(u8"\u2500\u2500")); break;  // |──|
-                case 0b0110: write(reinterpret_cast<const char*>(u8"\u250c\u2500")); break;  // |┌─|
-                case 0b0111: write(reinterpret_cast<const char*>(u8"\u252C\u2500")); break;  // |┬─|
-                case 0b1000: write(reinterpret_cast<const char*>(u8"\u2575 ")); break;  // |╵ |
-                case 0b1001: write(reinterpret_cast<const char*>(u8"\u2518 ")); break;  // |┘ |
-                case 0b1010: write(reinterpret_cast<const char*>(u8"\u2502 ")); break;  // |│ |
-                case 0b1011: write(reinterpret_cast<const char*>(u8"\u2524 ")); break;  // |┤ |
-                case 0b1100: write(reinterpret_cast<const char*>(u8"\u2514\u2500")); break;  // |└─|
-                case 0b1101: write(reinterpret_cast<const char*>(u8"\u2534\u2500")); break;  // |┴─|
-                case 0b1110: write(reinterpret_cast<const char*>(u8"\u251C\u2500")); break;  // |├─|
-                case 0b1111: write(reinterpret_cast<const char*>(u8"\u253C\u2500")); break;  // |┼─|
-            }
-        }
+    // clang-format off
+    constexpr std::array blocks = {
+        u8"  ",            // 0b0000 |  |
+        u8"\u2500 ",       // 0b0001 |─ |
+        u8"\u2577 ",       // 0b0010 |╷ |
+        u8"\u2510 ",       // 0b0011 |┐ |
+        u8" \u2500",       // 0b0100 | ─|
+        u8"\u2500\u2500",  // 0b0101 |──|
+        u8"\u250c\u2500",  // 0b0110 |┌─|
+        u8"\u252C\u2500",  // 0b0111 |┬─|
+        u8"\u2575 ",       // 0b1000 |╵ |
+        u8"\u2518 ",       // 0b1001 |┘ |
+        u8"\u2502 ",       // 0b1010 |│ |
+        u8"\u2524 ",       // 0b1011 |┤ |
+        u8"\u2514\u2500",  // 0b1100 |└─|
+        u8"\u2534\u2500",  // 0b1101 |┴─|
+        u8"\u251C\u2500",  // 0b1110 |├─|
+        u8"\u253C\u2500",  // 0b1111 |┼─|
+    };
+    // clang-format on
+
+    for (std::size_t row = 0; row < grid_height; ++row) {
+        for (std::size_t col = 0; col < grid_width; ++col)
+            write(reinterpret_cast<const char*>(blocks[grid[row][col].value()]));
 
         write('\n');
     }
